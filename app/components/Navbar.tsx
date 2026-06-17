5f7176ac05e3aa0f091935123d438fdfaaff002b
samaayit01_web <smile090453@gmail.com>
2026-06-17 15:44:28 +0700

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,29 +1,44 @@
+"use client";
 import Link from "next/link";
+import { use, useState } from "react";
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">    
-        <ul className="nav-links">
-            <li>
-                <Link href="/">Home</Link>
-            </li>
+    const [menuOpen, setMenuOpen] = useState(false);
 
-            <li>
-                <Link href="/about">About</Link>
-            </li>       
-            <li>
-                <Link href="/contact">Contact</Link>
-            </li>   
-            <li>
-                <Link href="/login">login</Link>
-            </li>  
-            <li>
-                <Link href="/regiter">regiter</Link>
-            </li>  
-            <li>
-                <Link href="/forgot-password">forgot-password</Link>
-            </li>  
-        </ul>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="nav-container">
+
+                <Link href="/" className="logo">
+                    ROBOT SHOP
+                </Link>
+
+                <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </button>
+
+                <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link href="/login">login</Link>
+                    </li>
+                    <li>
+                        <Link href="/regiter">regiter</Link>
+                    </li>
+                    <li>
+                        <Link href="/forgot-password">forgot-password</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
