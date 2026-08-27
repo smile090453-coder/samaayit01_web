ec8f809c0eeb3c277dbe4225672351a59db227ba
Ashwin Pondetrangsri <6729011014@cdti.ac.th>
2026-08-27 09:59:54 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import BlogCard from "@/components/BlogCard";
 import Image from "next/image";
-
 
 export default function Home() {
   return (
     <div >
-      <h2>สวัสดี เสื้อดำเด้าหน่อย</h2>
-        <p>โดย ภัทณุภา แสงแก้ว</p> 
-        <BlogCard/>  
+    
+      <h2> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h2>
+      <p> โดย สมเกียรติ ใจดี </p>
+ 
     </div>
   );
 }
